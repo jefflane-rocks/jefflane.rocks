b5873655be16ddd6312722f0afc2952a68d958e1
Jeff Lane <jeff@jeffreyrobertlane.com>
2025-07-21 15:54:41 -0700

    larger font

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div className="grid place-items-center h-screen bg-gray-900">
-      <div className="p-8 bg-black text-white rounded-lg shadow-lg text-6xl">
+      <div className="p-8 bg-black text-white rounded-lg shadow-lg text-7xl">
         JeffLane.Rocks
       </div>
     </div>
